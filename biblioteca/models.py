82894ec9cea2a91ba43e276086a9d69281b89039
Jox9 <149027884+Jox9@users.noreply.github.com>
2024-11-14 23:33:12 -0600

    Añada modelos a app biblioteca

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    nacionalidad = models.CharField(max_length=50)
+    edad = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.nombre}"
+
+
+class Libro(models.Model):
+    codigo = models.CharField(max_length=20)
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    genero = models.CharField(max_length=80)
+    idioma = models.CharField(max_length=50)
+    disponibilidad = models.BooleanField()
+    editorial = models.CharField(max_length=100)
+    fk_autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.titulo}"
+        
