c05880db032bc8bb92ce79ac70b128bb58225560
Jox9 <149027884+Jox9@users.noreply.github.com>
2024-11-16 01:34:29 -0600

    Añada vistas de libros y autores a bibliotecas

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def autores(request):
+    return render(request, "autores.html")
+
+def libros(request):
+    return render(request, "libros.html")
--- src/urls.py
+++ src/urls.py
@@ -4,9 +4,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-
+from biblioteca import views as biblioteca
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.pagina_inicial),
+    path("autores", biblioteca.autores),
+    path("libros", biblioteca.libros),
 ]
