8632507d47e56a8ad14344f135fc0c20fa7fd45a
Jox9 <149027884+Jox9@users.noreply.github.com>
2024-11-20 16:27:16 -0600

    contactenos: Corrija msj

--- contactenos/views.py
+++ contactenos/views.py
@@ -5,7 +5,7 @@ def contactenos(request):
 
     informacion = {
         "titulo": "Contactenos",
-        "msj": "Gracias por visitarnos, aquí están nuestros datos por si quieres visitarnos.",
+        "msj": "Gracias por visitarnos, aquí están nuestros datos por si quieres contactarnos.",
         "email": "admin@admin.com",
         "telefono": "12341234",
         "disponibilidad": True,
