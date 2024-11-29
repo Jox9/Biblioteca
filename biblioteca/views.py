ed2c8b4173c081b95802fd6bcc1927dbd6ee3809
Jox9 <149027884+Jox9@users.noreply.github.com>
2024-11-29 01:10:46 -0600

    Añada comentarios a vews

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from .models import Autor, Libro
 
 # Autores
-def autores(request): # lista de autores
+def autores(request): # Pagina que lista autores
 
-    # post
+    # Recibe solicitudes para crear autores
     if request.method == "POST":
         nombre = request.POST.get("nombre-autor")
         nacionalidad = request.POST.get("nacionalidad-autor")
@@ -14,20 +14,28 @@ def autores(request): # lista de autores
 
         autor.save()
 
-    # list data
+    # Datos, para listar autores
     autores_dt = Autor.objects.all()
     data = {"autores": autores_dt}
 
     return render(request, "autores.html", data)
 
 
-def crear_autor(request):
+def crear_autor(request): # Pagina para crear autores
     return render(request, "crear_autor.html")
 
-def editar_autor(request, id):
+def editar_autor(request, id): # Pagina para editar autores 
+   
+   # Datos para autocompletar formularios con el autor
+   # correspondiente. Ejm: editar autor Marcos  id = 1 
     autor_dt = Autor.objects.get(id=id)
     data = {"autor" : autor_dt}
+   
     return render(request, "editar_autor.html", data)
+
+def eliminar_autor():
+    pass
+
 
 # Libros
 def libros(request):
